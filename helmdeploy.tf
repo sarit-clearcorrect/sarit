7287cb54937939649deff65e6871e479a789352f
Sarit Panta <sarit.panta@clearcorrect.com>
2019-08-22 13:03:03 -0500

    this is test

---
+++ helmdeploy.tf
@@ -0,0 +1,11 @@
+provider "helm" {
+    kubernetes {
+        host     = "https://104.196.242.174"
+        username = "ClusterMaster"
+        password = "MindTheGap"
+
+        client_certificate     = "${file("~/.kube/client-cert.pem")}"
+        client_key             = "${file("~/.kube/client-key.pem")}"
+        cluster_ca_certificate = "${file("~/.kube/cluster-ca-cert.pem")}"
+    }
+}
